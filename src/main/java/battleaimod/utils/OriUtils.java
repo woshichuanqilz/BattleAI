d6024921bb768b673125d5ea8592cd25a54cc926
lovelife_lizhe <513278236@qq.com>
2024-06-04 21:44:20 +0800

    json read done

--- src/main/java/battleaimod/utils/OriUtils.java
+++ src/main/java/battleaimod/utils/OriUtils.java
@@ -1,6 +1,9 @@
 package battleaimod.utils;
 
 import battleaimod.GameData.EnemyType;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.google.gson.Gson;
 import com.megacrit.cardcrawl.localization.UIStrings;
@@ -13,14 +16,21 @@ import java.util.Map;
 
 public class OriUtils {
     public static ArrayList<String> getEnemyTypes(){
-        Gson gson = new Gson();
-
         try {
-            // Read the JSON file
+            ArrayList<EnemyType> enemy_types = new ArrayList<>();
             FileReader reader = new FileReader("EnemyType.json");
-
-            // read the content of key enemy_types
-            Map enemy_type_map = gson.fromJson(reader, Map.class);
+            Gson gson = new Gson();
+//            Map<String, EnemyType> enemy_type_map = gson.fromJson(reader, Map.class);
+            JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+            JsonArray enemy_types_json = null;
+            if (jsonObject.has("enemy_types")) {
+                enemy_types_json = jsonObject.get("enemy_types").getAsJsonArray();
+                // enemy_types_json to enemy_types
+                for (JsonElement enemy_type_json : enemy_types_json) {
+                    EnemyType enemyType = gson.fromJson(enemy_type_json, EnemyType.class);
+                    enemy_types.add(enemyType);
+                }
+            }
             System.out.println("test");
         } catch (IOException e) {
             e.printStackTrace();
